bda138e3a05a3f35da528910aa571be19ba4db6b
Harvard90873 <65774333+Harvard90873@users.noreply.github.com>
2021-06-12 09:56:09 +0200

    lazy load image

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -5,28 +5,28 @@ import Background from "../images/404.jpg";
 import { ReactComponent as Earth } from "../images/earth.svg";
 
 export default function NotFound() {
-    const history = useHistory();
+  const history = useHistory();
 
-    return (
-        <>
-            <Nav />
-            <div id="not-found-page" className="page-content">
-                <div className="not-found-content">
-                    <h1>
-                        4<Earth />4
-                    </h1>
-                    <h3>The page you were looking for was not found.</h3>
-                    <button
-                        onClick={() => {
-                            history.goBack();
-                        }}
-                        class="animated-btn"
-                    >
-                        Go Back
-                    </button>
-                </div>
-                <img id="not-found-bg" alt="404bg" src={Background} />
-            </div>
-        </>
-    );
+  return (
+    <>
+      <Nav />
+      <div id="not-found-page" className="page-content">
+        <div className="not-found-content">
+          <h1>
+            4<Earth />4
+          </h1>
+          <h3>The page you were looking for was not found.</h3>
+          <button
+            onClick={() => {
+              history.goBack();
+            }}
+            className="animated-btn"
+          >
+            Go Back
+          </button>
+        </div>
+        <img id="not-found-bg" alt="404bg" src={Background} />
+      </div>
+    </>
+  );
 }
