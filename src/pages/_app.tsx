f2c8c2e622c5ce7e5ba0931a9740990b2044c22e
Tim <harvard90873@gmail.com>
2021-10-26 17:58:57 +0200

    seo

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,6 +5,13 @@ import AOS from "aos";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { useEffect } from "react";
 import { Footer } from "../components/Footer";
+import Head from "next/head";
+
+const metadata = {
+  title: "Dream of Berlin",
+  image: "",
+  description: "Get to know about the awesome city of Berlin!",
+};
 
 const App = ({ Component, pageProps }: AppProps) => {
   useEffect(() => {
@@ -12,6 +19,31 @@ const App = ({ Component, pageProps }: AppProps) => {
   }, []);
   return (
     <>
+      <Head>
+        <title>{metadata.title}</title>
+        <meta name="title" content={metadata.title} />
+        <meta name="description" content={metadata.title} />
+        <meta name="keywords" content="auction, platform, trade" />
+        <meta name="robots" content="index, follow" />
+        <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
+        <meta name="language" content="English" />
+        <meta name="revisit-after" content="0 days" />
+        <meta name="author" content="Tim <timpersonal07@gmail.com>" />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content={metadata.title} />
+        <meta property="og:site_name" content={metadata.title} />
+        <meta property="og:url" content="https://dream-of-berlin.vercel.app" />
+        <meta property="og:image" content={metadata.image} />
+        <meta property="og:description" content={metadata.description} />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:site" content="@timthedev07" />
+        <meta name="twitter:title" content={metadata.title} />
+        <meta name="twitter:description" content={metadata.description} />
+        <meta name="twitter:image" content={metadata.image} />
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" href="/logo192.png" />
+      </Head>
+
       <Component {...pageProps} />
       <Footer />
     </>
